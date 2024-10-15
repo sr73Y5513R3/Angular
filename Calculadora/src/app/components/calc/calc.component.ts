07440389aeff1394053a6bac227bff0dc470c7a3
PabloTey <pablotey8@gmail.com>
2024-10-16 01:14:46 +0200

    terminao la calculadora

--- Calculadora/src/app/components/calc/calc.component.ts
+++ Calculadora/src/app/components/calc/calc.component.ts
@@ -7,35 +7,58 @@ import { Component } from '@angular/core';
 })
 export class CalcComponent {
 
-  //calculadora button
-  uno = "1";
-  dos = "2";
-  tres = "3";
-  cuatro = "4";
-  cinco = "5";
-  seis = "6";
-  siete = "7";
-  ocho = "8";
-  nueve = "9";
-  cero = "0";
-
-
-  valor = "0";
-  resultado = 0;
-  sumar = false
+  valor: string = "0";  
+  valor1: string = "0";  
+  resultado: number = 0;
+  operacion: string = ""; 
 
   addNumber (num: string){
-    this.valor = this.valor.concat(num);
+    if (this.valor === "0"){
+      this.valor = num;
+    }else{
+      this.valor = this.valor.concat(num);
+    }
   }
 
-  borrarImput(){
+  borrarInput(){
     this.valor = "0";
   }
-  
-  /*sumarNumeros(valor: String){
-    valor1= valor.;
-    this.sumar = true;
-  }*/
 
+  borrarUnNumero(){
+    if(this.valor.length<2){
+      this.valor= "0";
+    }else{
+      this.valor = this.valor.slice(0, -1);
+    }
+  }
   
+  seleccionarOperacion(op: string) {
+    this.valor1 = this.valor;  
+    this.operacion = op;  
+    this.valor = "0";  
+  }
+
+  calcularResultado() {
+    const num1 = Number(this.valor1);  
+    const num2 = Number(this.valor);
+
+    switch (this.operacion) {
+      case "+":
+        this.resultado = num1 + num2;
+        break;
+      case "-":
+        this.resultado = num1 - num2;
+        break;
+      default:
+        this.resultado = num2;
+    }  
+    this.valor = this.resultado.toString();
+    this.operacion = "";  
+  }
+
+  imgUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnZduW3SVmML3GcWWbKKYJbKpygrOPDc7Rtw&s";
+
+  easterEgg(){
+    alert("Que mirás bobo")
+  }
 }
